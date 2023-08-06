76af5f9ee8ed32709064f81e2a95bb806a27f80e
Manel-Os <oulds.manel@gmail.com>
2023-08-05 21:29:32 -0400

    user auth

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const csurf = require("csurf");
 const { isProduction } = require("./config/keys");
 
 require('./models/User');
+require('./config/passport'); 
+const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require("./routes/api/tweets");
@@ -43,6 +45,7 @@ app.use(
 app.use("/api/users", usersRouter);
 app.use("/api/tweets", tweetsRouter);
 app.use("/api/csrf", csrfRouter);
+app.use(passport.initialize());
 
 // Express custom middleware for catching all unmatched requests and formatting
 // a 404 error to be sent as the response.
